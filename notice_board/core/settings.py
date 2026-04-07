872344aaf825d3e045c0a4689eb8367f2ffcc06e
Ntlala <kingdomtechinnovations@gmail.com>
2026-04-08 02:46:17 +1300

    Save latest changes: MongoDB djongo config finalized

--- notice_board/core/settings.py
+++ notice_board/core/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from decouple import config
 
@@ -30,6 +31,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,6 +121,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
